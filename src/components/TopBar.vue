8c7d455eb9640d5a23912e41eb18221f4f55fb9d
tsutsu <tsutsu0928@gmail.com>
2019-08-04 12:40:16 +0900

    サンプル画面を作成

---
+++ src/components/TopBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#7986cb"
+      text-color="#fff"
+      active-text-color="#fff59d"
+    >
+      <el-menu-item index="1">カメラ映像</el-menu-item>
+      <el-menu-item>
+        <a href="https://github.com/y-tsutsu/yukari" target="_blank">GitHub</a>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopBar class="top-bar"></TopBar>
+    <el-image
+      class="main-image"
+      src="http://news.denfaminicogamer.jp/wp-content/uploads/2017/03/yu3.jpg"
+      fit="fill"
+    ></el-image>
+    <el-table class="data-table" :data="tableData" stripe>
+      <el-table-column prop="name" label="キャラクタ" width="180"></el-table-column>
+      <el-table-column prop="cv" label="声優" width="180"></el-table-column>
+      <el-table-column prop="note" label="性格"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TopBar from '@/components/TopBar.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    TopBar
+  },
+  data () {
+    return {
+      tableData: [
+        {
+          name: '野々原 ゆずこ',
+          cv: '大久保 瑠美',
+          note: '天真爛漫な性格で元気で無邪気な少女。いたずら好きでよく唯にしており、また奇妙な言動や発想も多い。しかし、成績は優秀で、時々急に真面目になることもある。'
+        },
+        {
+          name: '日向 縁',
+          cv: '種田 梨沙',
+          note: '家はお金持ち。天然でほんわかとしているが、時々勘が鋭いこともある。'
+        },
+        {
+          name: '櫟井 唯',
+          cv: '津田 美波',
+          note: '情報処理部の部員3人の中では一番の常識人でしっかり者。サバサバとした性格で言動もやや男性的であり、毎回暴走しがちなゆずこと天然な縁に対するツッコミ役でもある。高校2年から胸が急成長している。'
+        }
+      ]
+    }
   }
 }
 </script>
+
+<style scoped>
+.top-bar {
+  margin-bottom: 30px
+}
+.main-image {
+  margin-bottom: 30px
+}
+.data-table {
+  width: 60%;
+  margin: auto;
+}
+</style>
