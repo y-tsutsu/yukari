a05b23982bd11f5c8480dd6f28a8a17fb98dca2b
tsutsu <tsutsu0928@gmail.com>
2019-08-07 21:12:49 +0900

    ディレクトリ階層を変更

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-from api import api_bp
+from api.api import api_bp
 
 app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
 app.register_blueprint(api_bp)
