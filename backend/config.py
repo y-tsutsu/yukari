5b055ad6b296b49a025a440dc720c61c339f3979
Yusuke Tsutsumi <yusuke.tsutsumi@itage.co.jp>
2019-08-08 10:12:02 +0900

    映像の更新間隔をconfig.pyに定義

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,5 @@ class BaseConfig():
     RTSP_CAMERA = False
     MP4_CAMERA = True
     JPG_CAMERA = False
+
+    VIDEO_INTERVAL = 0.033
--- backend/video/views.py
+++ backend/video/views.py
@@ -8,11 +8,12 @@ video_bp = Blueprint('video', __name__, url_prefix='/video')
 
 
 def gen(camera):
+    from yukari import config
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        sleep(0.033)
+        sleep(config['VIDEO_INTERVAL'])
 
 
 @video_bp.route('/')
