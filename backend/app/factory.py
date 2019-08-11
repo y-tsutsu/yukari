cd75c712ef2fcafae762535114e1f660f2335f78
tsutsu <tsutsu0928@gmail.com>
2019-08-11 23:48:07 +0900

    appの生成関数をappディレクトリに移動

---
+++ backend/app/factory.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+from api.api import api_bp
+from app.views import app_bp
+from config import BaseConfig
+from videos.views import video_bp
+
+
+def create_app(name):
+    app = Flask(name, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
+    app.register_blueprint(api_bp)
+    app.register_blueprint(video_bp)
+    app.register_blueprint(app_bp)
+    app.config.from_object(BaseConfig)
+    return app
--- backend/yukari.py
+++ backend/yukari.py
@@ -1,23 +1,9 @@
-from flask import Flask
-
-from api.api import api_bp
-from app.views import app_bp
-from config import BaseConfig
+from app.factory import create_app
 from models.character import CharacterTable
-from videos.views import video_bp
-
-
-def create_app():
-    app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
-    app.register_blueprint(api_bp)
-    app.register_blueprint(video_bp)
-    app.register_blueprint(app_bp)
-    app.config.from_object(BaseConfig)
-    return app
 
 
 def main():
-    app = create_app()
+    app = create_app(__name__)
     CharacterTable.init_db(app)
     app.run(debug=True, host='0.0.0.0', port=80)
 
