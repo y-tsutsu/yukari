c6dbb4479065f191941dc99cacc8bedc641744c9
tsutsu <tsutsu0928@gmail.com>
2019-08-15 02:16:39 +0900

    カスケードファイルを追加

--- backend/image_process/factory.py
+++ backend/image_process/factory.py
@@ -6,13 +6,13 @@ from image_process.trimming import TrimmingProcess
 def create_image_processes(config):
     processes = []
     if config['REAL_FACE_IMG_PROC']:
-        processes.append(FacialRecognition('haarcascade_frontalface_default.xml'))
+        processes.append(FacialRecognition('haarcascades/haarcascade_frontalface_alt.xml'))
     if config['ANIME_FACE_IMG_PROC']:
-        processes.append(FacialRecognition('lbpcascade_animeface.xml'))
+        processes.append(FacialRecognition('haarcascades/lbpcascade_animeface.xml'))
     if config['DELAY_REAL_FACE_IMG_PROC']:
-        processes.append(DelayFacialRecognition('haarcascade_frontalface_default.xml'))
+        processes.append(DelayFacialRecognition('haarcascades/haarcascade_frontalface_alt.xml'))
     if config['DELAY_ANIME_FACE_IMG_PROC']:
-        processes.append(DelayFacialRecognition('lbpcascade_animeface.xml'))
+        processes.append(DelayFacialRecognition('haarcascades/lbpcascade_animeface.xml'))
     if config['TRIMMING_IMG_PROC']:
         processes.append(TrimmingProcess(800, 450))
     if config['DUMMY_IMG_PROC']:
