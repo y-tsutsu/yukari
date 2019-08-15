4def87c2e8e32df2146cd0802facd2b2cf2cecde
tsutsu <tsutsu0928@gmail.com>
2019-08-16 02:07:31 +0900

    アバウトページを更新

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-image class="main-image" src="/static/img/yukari.jpg" fit="fill"></el-image>
+
+    <h1>Yukari -- 縁 --</h1>
+    <P>
+      Networkカメラなどの映像に対して画像処理を行い，
+      その結果加工した映像や情報をリアルタイムにブラウザ上に表示するWebアプリのデモ．
+    </P>
+    <h2>ぽわぽわした天然娘。</h2>
+    <p>
+      本作の主人公の1人．情報処理部の部員の1人で，部長．栄太という兄がいる．<br>
+      家はお金持ち．天然でほんわかとしているが，時々勘が鋭いこともある．<br>
+      唯とは幼馴染で，小学2年生の遠足の時に，唯からお菓子をもらったことがきっかけで仲良くなる．<br>
+      親戚のおじさんに囲まれているからか，古いネタを放つこともあり，その際にはゆずこと唯には理解されないことが多い．<br>
+      ゆずこのテンションに便乗して，時々唯にいたずらをすることもある．
+    </p>
   </div>
 </template>
 
@@ -11,4 +25,15 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+}
+.main-image {
+  width: 45%;
+}
+.data-table {
+  width: 60%;
+  margin: auto;
+}
 </style>
